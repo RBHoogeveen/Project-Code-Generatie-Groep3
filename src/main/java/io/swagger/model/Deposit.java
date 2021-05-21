8320f7aff19449197b284194d6b116091a00e928
Ruben <rbhoogeveen@gmail.com>
2021-05-21 13:16:35 +0200

    Initial codegen

---
+++ src/main/java/io/swagger/model/Deposit.java
@@ -0,0 +1,206 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.model.Account;
+import io.swagger.model.User;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.math.BigDecimal;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * Deposit
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-05-21T10:25:28.299Z[GMT]")
+
+
+public class Deposit   {
+  @JsonProperty("id")
+  private Integer id = null;
+
+  @JsonProperty("amount")
+  private BigDecimal amount = null;
+
+  @JsonProperty("date")
+  private String date = null;
+
+  @JsonProperty("userPerforming")
+  private User userPerforming = null;
+
+  @JsonProperty("sender")
+  private Account sender = null;
+
+  @JsonProperty("reciever")
+  private Account reciever = null;
+
+  public Deposit id(Integer id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+   **/
+  @Schema(description = "")
+  
+    public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public Deposit amount(BigDecimal amount) {
+    this.amount = amount;
+    return this;
+  }
+
+  /**
+   * Get amount
+   * @return amount
+   **/
+  @Schema(description = "")
+  
+    @Valid
+    public BigDecimal getAmount() {
+    return amount;
+  }
+
+  public void setAmount(BigDecimal amount) {
+    this.amount = amount;
+  }
+
+  public Deposit date(String date) {
+    this.date = date;
+    return this;
+  }
+
+  /**
+   * Get date
+   * @return date
+   **/
+  @Schema(example = "14-05-2021", description = "")
+  
+    public String getDate() {
+    return date;
+  }
+
+  public void setDate(String date) {
+    this.date = date;
+  }
+
+  public Deposit userPerforming(User userPerforming) {
+    this.userPerforming = userPerforming;
+    return this;
+  }
+
+  /**
+   * Get userPerforming
+   * @return userPerforming
+   **/
+  @Schema(description = "")
+  
+    @Valid
+    public User getUserPerforming() {
+    return userPerforming;
+  }
+
+  public void setUserPerforming(User userPerforming) {
+    this.userPerforming = userPerforming;
+  }
+
+  public Deposit sender(Account sender) {
+    this.sender = sender;
+    return this;
+  }
+
+  /**
+   * Get sender
+   * @return sender
+   **/
+  @Schema(description = "")
+  
+    @Valid
+    public Account getSender() {
+    return sender;
+  }
+
+  public void setSender(Account sender) {
+    this.sender = sender;
+  }
+
+  public Deposit reciever(Account reciever) {
+    this.reciever = reciever;
+    return this;
+  }
+
+  /**
+   * Get reciever
+   * @return reciever
+   **/
+  @Schema(description = "")
+  
+    @Valid
+    public Account getReciever() {
+    return reciever;
+  }
+
+  public void setReciever(Account reciever) {
+    this.reciever = reciever;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Deposit deposit = (Deposit) o;
+    return Objects.equals(this.id, deposit.id) &&
+        Objects.equals(this.amount, deposit.amount) &&
+        Objects.equals(this.date, deposit.date) &&
+        Objects.equals(this.userPerforming, deposit.userPerforming) &&
+        Objects.equals(this.sender, deposit.sender) &&
+        Objects.equals(this.reciever, deposit.reciever);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, amount, date, userPerforming, sender, reciever);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Deposit {\n");
+    
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    amount: ").append(toIndentedString(amount)).append("\n");
+    sb.append("    date: ").append(toIndentedString(date)).append("\n");
+    sb.append("    userPerforming: ").append(toIndentedString(userPerforming)).append("\n");
+    sb.append("    sender: ").append(toIndentedString(sender)).append("\n");
+    sb.append("    reciever: ").append(toIndentedString(reciever)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
