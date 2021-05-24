d1399f6eb36142c9eb09f3606e178f40fcca0a7e
Ruben Schild <650580@student.inholland.nl>
2021-05-24 13:49:12 +0200

    controller mapje en exception mapje aangemaakt

--- src/main/java/io/swagger/api/controller/AccountApiController.java
+++ src/main/java/io/swagger/api/controller/AccountApiController.java
@@ -1,25 +1,20 @@
-package io.swagger.api;
+package io.swagger.api.controller;
 
 import java.math.BigDecimal;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.*;
+import io.swagger.api.AccountApi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.*;
 import javax.validation.Valid;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.util.List;
+
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2021-05-21T11:36:55.738Z")
 
 @Controller
@@ -51,5 +46,4 @@ public class AccountApiController implements AccountApi {
         String accept = request.getHeader("Accept");
         return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
     }
-
 }
