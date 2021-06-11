4422866d0ccc34a3138c61e061ae3f8ae83774be
Ruben Schild <650580@student.inholland.nl>
2021-06-11 12:23:20 +0200

    dayspent uit database gehaald, daylimit korter gemaakt

--- src/main/java/io/swagger/repository/TransactionRepository.java
+++ src/main/java/io/swagger/repository/TransactionRepository.java
@@ -1,6 +1,7 @@
 package io.swagger.repository;
 
 import io.swagger.model.Transaction;
+import io.swagger.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,6 @@ import java.util.List;
 public interface TransactionRepository extends JpaRepository<Transaction, Long>{
     @Query(value = "SELECT * FROM Transaction WHERE user_performing_id = ?1", nativeQuery = true)
     List<Transaction> getTransactionsByUser(Integer userId);
+
+    List<Transaction> getAllByUserPerforming(User userPerforming);
 }
