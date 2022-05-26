9e1623c406836279509f7ee9f44c21be04a99f6e
Ruben Schild <650580@student.inholland.nl>
2022-05-26 14:36:15 +0200

    Added repo tests

---
+++ src/test/java/io/swagger/repository/TransactionRepositoryTests.java
@@ -0,0 +1,54 @@
+package io.swagger.repository;
+
+import io.swagger.model.Account;
+import io.swagger.model.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class TransactionRepositoryTests {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TransactionRepository transactionRepo;
+
+    @Autowired
+    private AccountRepository accountRepo;
+
+    @Test
+    public void PostTransaction_ShouldReturnTransaction() {
+        // Prepare
+        Transaction newTransaction = new Transaction();
+        transactionRepo.save(newTransaction);
+
+        // Then
+        Transaction transaction = transactionRepo.getTransactionById(newTransaction.getId());
+
+        // Assert
+        assertThat(transaction).isEqualTo(newTransaction);
+    }
+
+    @Test
+    public void UpdateBalance_ShouldReturnUpdatedBalance() {
+        // Prepare
+        Account account = new Account();
+        account.setBalance(new BigDecimal("500.00"));
+        account.setType(false);
+        account.setIban("NL19INHO0123456789");
+        accountRepo.save(account);
+        BigDecimal newBalance = new BigDecimal("999.00");
+
+        // Then
+        transactionRepo.UpdateBalance(newBalance, "NL19INHO0123456789", false);
+        Account performer = accountRepo.getAccountByIban("NL19INHO0123456789");
+
+        // Assert
+        assertThat(performer.getBalance()).isEqualTo(newBalance);
+    }
+}
