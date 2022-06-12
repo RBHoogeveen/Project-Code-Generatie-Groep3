67e5796cb5da1d7e38fc1d704dfa52cbff6196e9
CyberPim <54932011+CyberPim@users.noreply.github.com>
2022-06-12 12:02:55 +0200

    Submission 2 (#45)
    * Fixed issues with tests
    
    * Changed and added test
    
    * Update AccountControllerTest.java
    
    * Update TransactionControllerTest.java
    
    Co-authored-by: d <650508@student.inholland.nl>

--- src/test/java/io/swagger/Controller/AccountControllerTest.java
+++ src/test/java/io/swagger/Controller/AccountControllerTest.java
@@ -19,7 +19,9 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import java.math.BigDecimal;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest()
@@ -55,14 +57,15 @@ public class AccountControllerTest {
     admin.put("password", "Admin");
 
     MvcResult result = this.mockMvc.perform(post("/login")
-        .contentType(MediaType.APPLICATION_JSON).content(admin.toString()))
-        .andExpect(status().isOk())
-        .andReturn();
+            .contentType(MediaType.APPLICATION_JSON).content(admin.toString()))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("token").exists())
+            .andReturn();
 
     //Save token and user id for performing tests
     String content = result.getResponse().getContentAsString();
     this.xAuthTokenAdmin = content.substring(10, content.indexOf('}') - 1);
-    JSONObject adminUser = new JSONObject(result.getResponse().getContentAsString());
+//    JSONObject adminUser = new JSONObject(result.getResponse().getContentAsString());
 
     //Get user with employee access
     JSONObject user = new JSONObject();
@@ -70,21 +73,25 @@ public class AccountControllerTest {
     user.put("password", "User");
 
     result = this.mockMvc.perform(post("/login")
-        .contentType(MediaType.APPLICATION_JSON).content(user.toString()))
-        .andExpect(status().isOk())
-        .andReturn();
+            .contentType(MediaType.APPLICATION_JSON).content(user.toString()))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("token").exists())
+            .andReturn();
 
     String userContent = result.getResponse().getContentAsString();
     this.xAuthTokenUser = userContent.substring(10, userContent.indexOf('}') - 1);
-    user = new JSONObject(result.getResponse().getContentAsString());
+//    user = new JSONObject(result.getResponse().getContentAsString());
   }
 
   @Test
   @DisplayName("Checks if the admin can get the Admins accounts")
   public void getAccountsByUsernameShouldReturnOk() throws Exception {
     this.mockMvc.perform(get("/accounts/Admin")
-        .header(HEADER_STRING, TOKEN_PREFIX + " " + this.xAuthTokenAdmin))
-        .andExpect(status().isOk());
+            .header(HEADER_STRING, TOKEN_PREFIX + " " + this.xAuthTokenAdmin))
+            .andExpect(jsonPath("$", hasSize(2)))
+            .andExpect(jsonPath("$[0].iban").value("NL02INHO0000000002"))
+            .andExpect(jsonPath("$[1].iban").value("NL03INHO0000000003"))
+            .andExpect(status().isOk());
   }
 
   @Test
@@ -98,29 +105,52 @@ public class AccountControllerTest {
     createAccount.setUsername("Test");
 
     this.mockMvc.perform(post("/accounts")
-        .header(HEADER_STRING, TOKEN_PREFIX + " " + this.xAuthTokenAdmin)
-        .content(asJsonString(createAccount))
-        .contentType(MediaType.APPLICATION_JSON)
-        .accept(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk());
+            .header(HEADER_STRING, TOKEN_PREFIX + " " + this.xAuthTokenAdmin)
+            .content(asJsonString(createAccount))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("isActive").value("true"));
   }
 
   @Test
   @DisplayName("Checks if the user of role admin is able to UPDATE an account")
-  public void updatingAnAccountShouldReturnOK() throws Exception {
+  public void updatingAnAccountAsAdminShouldReturnOK() throws Exception {
     CreateUpdateAccountDTO updateAccount = new CreateUpdateAccountDTO();
     updateAccount.setType(false);
     updateAccount.setAbsoluteLimit(BigDecimal.ZERO);
     updateAccount.setIsActive(false);
-    updateAccount.setBalance(BigDecimal.valueOf(2000));
+    updateAccount.setBalance(BigDecimal.valueOf(123));
     updateAccount.setUsername("Admin");
 
     this.mockMvc.perform(put("/accounts/NL02INHO0000000002")
-        .header(HEADER_STRING, TOKEN_PREFIX + " " + this.xAuthTokenAdmin)
-        .content(asJsonString(updateAccount))
-        .contentType(MediaType.APPLICATION_JSON)
-        .accept(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk());
+            .header(HEADER_STRING, TOKEN_PREFIX + " " + this.xAuthTokenAdmin)
+            .content(asJsonString(updateAccount))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("isActive").value("false"))
+            .andExpect(jsonPath("iban").value("NL02INHO0000000002"))
+            .andExpect(jsonPath("balance").value("123"))
+            .andExpect(status().isOk());
+  }
+
+  @Test
+  @DisplayName("Checks if the user of role admin is able to UPDATE an account")
+  public void updatingAnAccountAsUserShouldReturnError() throws Exception {
+    CreateUpdateAccountDTO updateAccount = new CreateUpdateAccountDTO();
+    updateAccount.setType(false);
+    updateAccount.setAbsoluteLimit(BigDecimal.ZERO);
+    updateAccount.setIsActive(false);
+    updateAccount.setBalance(BigDecimal.valueOf(123));
+    updateAccount.setUsername("User");
+
+    this.mockMvc.perform(put("/accounts/NL02INHO0000000002")
+            .header(HEADER_STRING, TOKEN_PREFIX + " " + this.xAuthTokenUser)
+            .content(asJsonString(updateAccount))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("message").value("Access is denied"))
+            .andExpect(status().is(400));
   }
 
 
